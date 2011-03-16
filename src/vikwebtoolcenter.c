9b3aa3f96fb3e7cd71626c21ec8612772905bedc
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2011-03-16 22:56:16 +0100

    Replace code with GObject macro (vikwebtoolcenter.c)

--- src/vikwebtoolcenter.c
+++ src/vikwebtoolcenter.c
@@ -33,10 +33,8 @@
 #include "util.h"
 #include "globals.h"
 
-static void webtool_center_class_init ( VikWebtoolCenterClass *klass );
-static void webtool_center_init ( VikWebtoolCenter *vwd );
-
-static GObjectClass *parent_class;
+static void vik_webtool_center_class_init ( VikWebtoolCenterClass *klass );
+static void vik_webtool_center_init ( VikWebtoolCenter *vwd );
 
 static void webtool_center_finalize ( GObject *gob );
 
@@ -54,29 +52,7 @@ struct _VikWebtoolCenterPrivate
                                        VIK_WEBTOOL_CENTER_TYPE,          \
                                        VikWebtoolCenterPrivate))
 
-GType vik_webtool_center_get_type()
-{
-  static GType w_type = 0;
-
-  if (!w_type)
-  {
-    static const GTypeInfo w_info = 
-    {
-      sizeof (VikWebtoolCenterClass),
-      NULL, /* base_init */
-      NULL, /* base_finalize */
-      (GClassInitFunc) webtool_center_class_init,
-      NULL, /* class_finalize */
-      NULL, /* class_data */
-      sizeof (VikWebtoolCenter),
-      0,
-      (GInstanceInitFunc) webtool_center_init,
-    };
-    w_type = g_type_register_static ( VIK_WEBTOOL_TYPE, "VikWebtoolCenter", &w_info, 0 );
-  }
-
-  return w_type;
-}
+G_DEFINE_TYPE (VikWebtoolCenter, vik_webtool_center, VIK_WEBTOOL_TYPE);
 
 enum
 {
@@ -131,7 +107,8 @@ webtool_center_get_property (GObject    *object,
     }
 }
 
-static void webtool_center_class_init ( VikWebtoolCenterClass *klass )
+static void
+vik_webtool_center_class_init ( VikWebtoolCenterClass *klass )
 {
   GObjectClass *gobject_class;
   VikWebtoolClass *base_class;
@@ -151,8 +128,6 @@ static void webtool_center_class_init ( VikWebtoolCenterClass *klass )
   g_object_class_install_property (gobject_class,
                                    PROP_URL,
                                    pspec);
-
-  parent_class = g_type_class_peek_parent (klass);
 
   base_class = VIK_WEBTOOL_CLASS ( klass );
   base_class->get_url = webtool_center_get_url;
@@ -177,7 +152,8 @@ VikWebtoolCenter *vik_webtool_center_new_with_members ( const gchar *label, cons
   return result;
 }
 
-static void webtool_center_init ( VikWebtoolCenter *self )
+static void
+vik_webtool_center_init ( VikWebtoolCenter *self )
 {
   VikWebtoolCenterPrivate *priv = WEBTOOL_CENTER_GET_PRIVATE (self);
   priv->url = NULL;
@@ -187,7 +163,7 @@ static void webtool_center_finalize ( GObject *gob )
 {
   VikWebtoolCenterPrivate *priv = WEBTOOL_CENTER_GET_PRIVATE ( gob );
   g_free ( priv->url ); priv->url = NULL;
-  G_OBJECT_CLASS(parent_class)->finalize(gob);
+  G_OBJECT_CLASS(vik_webtool_center_parent_class)->finalize(gob);
 }
 
 /* 1 << (x) is like a 2**(x) */
