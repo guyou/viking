f9dd429c17d5652d2a0ef3bb9232bfd6671057d9
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2013-10-22 22:25:12 +0200

    Improve test util

--- test/test_babel.c
+++ test/test_babel.c
@@ -13,8 +13,11 @@ int main(int argc, char*argv[])
 {
 	a_babel_init();
 
-	BabelMode mode = { 0,0,1,0,1,0 };
+	if (argc != 7) return 1;
+	BabelMode mode = { atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]) };
 	a_babel_foreach_file_with_mode(mode, print_file_format, NULL);
 
 	a_babel_uninit();
+
+	return 0;
 }
